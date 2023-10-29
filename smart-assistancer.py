e2089351acb61a727090f321e79e287d52b22542
rock2ayush <144557464+rock2ayush@users.noreply.github.com>
2023-10-29 10:09:22 +0530

    Update smart-assistancer.py

--- smart-assistancer.py
+++ smart-assistancer.py
@@ -25,4 +25,50 @@ Button(text="Close",command=root.destroy).pack(fill=X)
 
 
 
+
 def click(event):
+    global scvalue
+    text=event.widget.cget("text")
+    print(text)
+    speak(text)
+    if text=="=":
+
+        if scvalue.get().isdigit():
+            value=int(scvalue.get())
+            speak(str(value))
+        else:
+            try:
+                value = eval(screen.get())
+                speak(str(value))
+            except Exception as e:
+                print(e)
+                scvalue.set("Syntax Error")
+                speak("Syntax Error")
+                screen.update()
+        scvalue.set(value)
+        screen.update()
+    elif text=="Time":
+        def time():
+            string=strftime("%H:%M:%S %p")
+            speak(string)
+            scvalue.set(string)
+            screen.update()
+        time()
+        
+    elif text=="Date":
+        def date():
+            string=strftime('%d-%m-%Y')
+            # string=strftime("%")
+            speak(string)
+            scvalue.set(string)
+            screen.update()
+        date()
+    elif text=="Speak here !":
+
+        def add():
+            global temp
+            temp=0
+            for x in range(length):
+                temp=temp+list[x]
+            return(temp)
+
